7dcf8030a758ee665aea3d11a9f50a5713f0c56c
Mikita Makeichyk <mikitamakeichyk@gmail.com>
2025-10-24 19:29:58 +0400

    feat: implement list function

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,20 @@
+import { readdir } from 'node:fs/promises';
+import path from 'node:path';
+
 const list = async () => {
-  // Write your code here
+  // list.js - implement function that prints array of all filenames from files folder into console (if files folder doesn't exists Error with message FS operation failed must be thrown)
+  const pathToRead = path.resolve('src', 'fs', 'files');
+
+  try {
+    const listOfFiles = await readdir(pathToRead);
+    console.log(listOfFiles);
+  } catch (error) {
+    if (error.code === 'ENOENT') {
+      throw new Error('FS operation failed');
+    } else {
+      throw error;
+    }
+  }
 };
 
 await list();
