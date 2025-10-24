52ffb564c38a54e1b902187bdc667a6b7d87dffb
Mikita Makeichyk <mikitamakeichyk@gmail.com>
2025-10-24 19:19:59 +0400

    feat: implement remove function

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,21 @@
+import { rm } from 'node:fs/promises';
+import path from 'node:path';
+
 const remove = async () => {
-  // Write your code here
+  // delete.js - implement function that deletes file fileToRemove.txt (if there's no file fileToRemove.txt Error with message FS operation failed must be thrown)
+  const filename = 'fileToRemove.txt';
+  const pathToRemove = path.resolve('src', 'fs', 'files', filename);
+
+  try {
+    await rm(pathToRemove);
+    console.log('Successfully removed a file');
+  } catch (error) {
+    if (error.code === 'ENOENT') {
+      throw new Error('FS operation failed');
+    } else {
+      throw error;
+    }
+  }
 };
 
 await remove();
