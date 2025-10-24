4c1130b8302072cf96fd377d59f1864904fbe09d
Mikita Makeichyk <mikitamakeichyk@gmail.com>
2025-10-24 17:50:07 +0400

    init: start nodejs-basics task

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,22 @@
+import { writeFile } from 'node:fs/promises';
+import path from 'node:path';
+
 const create = async () => {
-  // Write your code here
+  //create.js - implement function that creates new file fresh.txt with content I am fresh and young inside of the files folder (if file already exists Error with message FS operation failed must be thrown)
+  const fileName = 'fresh.txt';
+  const filePath = path.resolve('src', 'fs', 'files', fileName);
+  const content = 'I am fresh and young';
+
+  try {
+    await writeFile(filePath, content, { flag: 'wx' });
+    console.log('Successfully created a file');
+  } catch (error) {
+    if (error.code === 'EEXIST') {
+      throw new Error('FS operation failed');
+    } else {
+      throw error;
+    }
+  }
 };
 
 await create();
