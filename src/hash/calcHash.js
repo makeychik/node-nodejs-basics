6deec1c2806ffda391626321de2d7ec8f667f58e
Mikita Makeichyk <mikitamakeichyk@gmail.com>
2025-10-25 21:34:43 +0400

    feat: implement hash function

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,22 @@
+import crypto from 'node:crypto';
+import { createReadStream, createWriteStream } from 'node:fs';
+import path from 'node:path';
+
 const calculateHash = async () => {
-  // Write your code here
+  // calcHash.js - implement function that calculates SHA256 hash for file fileToCalculateHashFor.txt and logs it into console as hex using Streams API
+  const filePath = path.resolve(
+    'src',
+    'hash',
+    'files',
+    'fileToCalculateHashFor.txt'
+  );
+
+  const hash = crypto.createHash('sha256');
+  hash.setEncoding('hex');
+
+  createReadStream(filePath)
+    .pipe(hash)
+    .on('finish', () => console.log(hash.read()));
 };
 
 await calculateHash();
