875674a5f741cad667b454a5f44b3265d59486ab
Mikita Makeichyk <mikitamakeichyk@gmail.com>
2025-10-25 17:37:36 +0400

    feat: implement args function

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,13 @@
 const parseArgs = () => {
-  // Write your code here
+  // args.js - implement function that parses command line arguments (given in format --propName value --prop2Name value2, you don't need to validate it) and prints them to the console in the format propName is value, prop2Name is value2
+  const argumentsArray = process.argv;
+  const resultStrings = argumentsArray.reduce((result, current, index) => {
+    if (current.startsWith('--')) {
+      result.push(`${current.slice(2)} is ${argumentsArray[index + 1]}`);
+    }
+    return result;
+  }, []);
+  console.log(resultStrings.join(', '));
 };
 
 parseArgs();
