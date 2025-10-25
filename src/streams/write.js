d40d1c361387f5a3fdaf15c6b4cb839b00ed1f50
Mikita Makeichyk <mikitamakeichyk@gmail.com>
2025-10-26 00:56:57 +0400

    feat: implement streams:write function

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,10 @@
+import { createWriteStream } from 'node:fs';
+import path from 'node:path';
+
 const write = async () => {
-  // Write your code here
+  // write.js - implement function that writes process.stdin data into file fileToWrite.txt content using Writable Stream
+  const filePath = path.resolve('src', 'streams', 'files', 'fileToWrite.txt');
+  process.stdin.pipe(createWriteStream(filePath));
 };
 
 await write();
