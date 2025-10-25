fb7353d5faf43dfea50749efdfb546fa0022cfb0
Mikita Makeichyk <mikitamakeichyk@gmail.com>
2025-10-26 00:48:30 +0400

    feat: implement streams:read function

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,10 @@
+import { createReadStream } from 'node:fs';
+import path from 'node:path';
+
 const read = async () => {
-  // Write your code here
+  // read.js - implement function that reads file fileToRead.txt content using Readable Stream and prints it's content into process.stdout
+  const filePath = path.resolve('src', 'streams', 'files', 'fileToRead.txt');
+  createReadStream(filePath).pipe(process.stdout);
 };
 
 await read();
