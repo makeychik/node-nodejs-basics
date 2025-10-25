1c8687788379c1d0e7a999336575135b1be8f90d
Mikita Makeichyk <mikitamakeichyk@gmail.com>
2025-10-26 01:55:10 +0400

    refactor: use { end: false } and await once to make streams:read function work with npm run

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,10 +1,17 @@
+import { once } from 'node:events';
 import { createReadStream } from 'node:fs';
 import path from 'node:path';
+import { pipeline } from 'node:stream/promises';
 
 const read = async () => {
   // read.js - implement function that reads file fileToRead.txt content using Readable Stream and prints it's content into process.stdout
   const filePath = path.resolve('src', 'streams', 'files', 'fileToRead.txt');
-  createReadStream(filePath).pipe(process.stdout);
+  const readStream = createReadStream(filePath);
+  readStream.pipe(process.stdout, {
+    end: false,
+  });
+  await once(readStream, 'end');
+  process.stdout.write('\n');
 };
 
 await read();
