1388f3169af212153988b1fd4b2c0f8b800b91a2
Mikita Makeichyk <mikitamakeichyk@gmail.com>
2025-10-25 17:29:55 +0400

    feat: implement env function

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,14 @@
 const parseEnv = () => {
-  // Write your code here
+  // env.js - implement function that parses environment variables with prefix RSS_ and prints them to the console in the format RSS_name1=value1; RSS_name2=value2
+  const entries = Object.entries(process.env);
+  const resultStrings = entries.reduce((result, [key, value]) => {
+    if (key.startsWith('RSS_')) {
+      result.push(`${key} is ${value}`);
+    }
+    return result;
+  }, []);
+
+  console.log(resultStrings.join(', '));
 };
 
 parseEnv();
