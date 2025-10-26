511d9ba311717b067e15e5865f7f47ec5cca4d5c
Mikita Makeichyk <mikitamakeichyk@gmail.com>
2025-10-26 14:25:07 +0400

    feat: implement compress function

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,14 @@
+import { createReadStream, createWriteStream } from 'node:fs';
+import { createGzip } from 'node:zlib';
+import path from 'node:path';
+
 const compress = async () => {
-  // Write your code here
+  // compress.js - implement function that compresses file fileToCompress.txt to archive.gz using zlib and Streams API
+  const filePath = path.resolve('src', 'zip', 'files', 'fileToCompress.txt');
+  const zipPath = path.resolve('src', 'zip', 'files', 'archive.gz');
+  createReadStream(filePath)
+    .pipe(createGzip())
+    .pipe(createWriteStream(zipPath));
 };
 
 await compress();
